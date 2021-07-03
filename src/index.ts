978f6c3982e1d3119dbee0103b40ba06637a238f
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2021-07-03 15:17:33 +0530

    feat: trip type

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+declare module Types {
+  export interface Trip {
+    id: string;
+    destination: string;
+    title: string;
+    description: string;
+    cost: number;
+    createdAt: string;
+    updatedAt: string;
+  }
+}
